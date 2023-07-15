467f9943d4416e2f1f6ce9624cb7f66aaa84adf6
MarkosComK <markoscomks@gmail.com>
2023-07-15 11:26:52 +0100

    setting up tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-screen flex justify-center items-center ">
+      <section className="max-w-840 w-11/12 border-solid border-2 border-red-700">
+        <div className="">
+          <div>DAY</div>
+          <div>MONTH</div>
+          <div>YEAR</div>
+        </div>
+        <div>
+          <div>
+            --------------
+          </div>
+          <div>
+            ARROW
+          </div>
+        </div>
+        <div>
+          <div>38 years</div>
+          <div>3 months</div>
+          <div>26 days</div>
+        </div>
+      </section>
     </div>
   );
 }
