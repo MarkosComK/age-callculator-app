1395d8999f014086f171a6c9263d30f48a21a13e
MarkosComK <markoscomks@gmail.com>
2023-07-16 13:21:00 +0100

    color issues solved

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
   const [month, setMonth] = useState(1)
   const [year, setYear] = useState(1900)
 
-  const [years, setYears] = useState(0)
-  const [months, setMonths] = useState(0)
-  const [days, setDays] = useState(0)
+  const [years, setYears] = useState("- -")
+  const [months, setMonths] = useState("- -")
+  const [days, setDays] = useState("- -")
 
   const handleDay = (event) => {
     setDay(event.target.value)
@@ -32,15 +32,15 @@ function App() {
   }
 
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-gray-100">
+    <main className="w-full h-screen flex justify-center items-center bg-gray-100">
       <section className="max-w-840 w-11/12 sm:h-651  flex justify-center items-center flex-col bg-white sm:rounded-br-200 rounded-br-100 rounded-3xl p-6 sm:p-16">
-        <div className="w-full h-101 flex  gap-x-0 sm:gap-x-9">
+        <main className="w-full h-101 flex  gap-x-0 sm:gap-x-9">
           <div className="w-40">
             <p>
               Day
             </p>
             <fieldset>
-              <input className="md:w-36 sm:w-32 w-24 text-32 font-bold p-4 @apply text-base border p-3 rounded-lg border-[#cccccc] border-solid;" type="number" min={1} max={31} placeholder="DD" onChange={handleDay}/>
+              <input className="md:w-36 sm:w-32 w-24 sm:text-32 font-bold p-4 @apply text-base border p-3 rounded-lg border-[#cccccc] border-solid;" type="number" min={1} max={31} placeholder="DD" onChange={handleDay}/>
             </fieldset>
           </div>
           <div className="w-40">
@@ -48,7 +48,7 @@ function App() {
               Month
             </p>
             <fieldset>
-              <input className="md:w-36 sm:w-32 w-24 text-32 font-bold p-4 @apply text-base border p-3 rounded-lg border-[#cccccc] border-solid;" type="number" min={1} max={12} placeholder="MM" onChange={handleMonth}/>
+              <input className="md:w-36 sm:w-32 w-24 sm:text-32 font-bold p-4 @apply text-base border p-3 rounded-lg border-[#cccccc] border-solid;" type="number" min={1} max={12} placeholder="MM" onChange={handleMonth}/>
             </fieldset>
           </div>
           <div className="w-40">
@@ -56,10 +56,10 @@ function App() {
               Year
             </p>
             <fieldset>
-              <input className="md:w-36 sm:w-32 w-24 text-32 font-bold p-4 @apply text-base border p-3 rounded-lg border-[#cccccc] border-solid;" type="number" min={1900} max={2023} placeholder="YYYY" onChange={handleYear}/>
+              <input className="md:w-36 sm:w-32 w-24 sm:text-32 font-bold p-4 @apply text-base border p-3 rounded-lg border-[#cccccc] border-solid;" type="number" min={1900} max={2023} placeholder="YYYY" onChange={handleYear}/>
             </fieldset>
           </div>
-        </div>
+        </main>
         <div className="w-full sm:h-24 flex items-center">
           <div className="w-full h-px bg-gray-500 relative"></div>
           <div className=" top-0">
@@ -69,12 +69,12 @@ function App() {
           </div>
         </div>
         <div className="sm:h-342 w-full flex flex-col justify-center md:text-9xl  sm:text-8xl text-6xl text-left font-bold">
-          <div className="md:h-1/3 sm:h-20 h-16"><span className="text-indigo-600">{years} </span>years</div>
-          <div className="md:h-1/3 sm:h-20 h-16"><span className="text-indigo-600">{months} </span>months</div>
-          <div className="md:h-1/3 sm:h-20 h-16"><span className="text-indigo-600">{days} </span>days</div>
+          <div className="md:h-1/3 sm:h-20 h-16"><span className="text-purple">{years} </span>years</div>
+          <div className="md:h-1/3 sm:h-20 h-16"><span className="text-purple">{months} </span>months</div>
+          <div className="md:h-1/3 sm:h-20 h-16"><span className="text-purple">{days} </span>days</div>
         </div>
       </section>
-    </div>
+    </main>
   );
 }
 
